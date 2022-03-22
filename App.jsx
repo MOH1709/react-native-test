8cabeeaf4c0fefbddeedf3c79ed7cb9073aca602
MOH1709 <ahirwalmohit3216@gmail.com>
2022-03-22 21:59:37 +0530

    save text to local storage

--- App.jsx
+++ App.jsx
@@ -1,5 +1,8 @@
 import { useState, useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+
+//-----------------------------------------------> installed
+import * as SecureStore from "expo-secure-store";
 
 //-----------------------------------------------> custom components
 import Test from "./src/Test";
@@ -9,12 +12,17 @@ import { StatusBar } from "expo-status-bar";
 //-----------------------------------------------> default export
 export default function App() {
   const [res, setRes] = useState({});
+  const [input, setInput] = useState({
+    key: "",
+    value: "",
+  });
+  const [result, setResult] = useState("none");
 
-  //request server, on load App component
+  //  request server, on load App component
   useEffect(() => {
     let isMounted = true;
 
-    //send request to test-server
+    //  send request to test-server
     const getRes = async () => {
       try {
         const res = await axios.get("/");
@@ -30,11 +38,75 @@ export default function App() {
     };
   }, []);
 
-  //returned component
+  //  save entered key value pair
+  const save = async (key, val) => {
+    try {
+      await SecureStore.setItemAsync(key, val);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  //  get value of entered key
+  const getVal = async (key) => {
+    try {
+      const response = await SecureStore.getItemAsync(key);
+      setResult(response || "Invalid Key");
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  //  on change input handler
+  const onChangeHandler = (name, value) => {
+    setInput({
+      ...input,
+      [name]: value,
+    });
+  };
+
+  //  returned component
   return (
     <View style={styles.container}>
       <Test />
-      <Text>{res?.msg}</Text>
+
+      {/* set key value pairs */}
+      <TextInput
+        style={styles.input}
+        placeholder="Enter key"
+        value={input.key}
+        onChangeText={(value) => onChangeHandler("key", value)}
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Enter value"
+        value={input.value}
+        onChangeText={(value) => onChangeHandler("value", value)}
+      />
+
+      <Button
+        title="save"
+        onPress={() => {
+          save(input.key, input.value);
+          setInput({ key: "", value: "" });
+        }}
+      />
+
+      {/* get value of entered key */}
+      <Text style={styles.h1}>Get Value</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter key"
+        onSubmitEditing={(e) => getVal(e.nativeEvent.text)}
+      />
+      <Text>
+        Value of Entered Key : <Text style={styles.result}>{result}</Text>{" "}
+      </Text>
+
+      <Text>
+        Response from server : <Text style={styles.result}>{res?.msg}</Text>
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -44,8 +116,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginHorizontal: "auto",
+    paddingHorizontal: 20,
     backgroundColor: "#fff",
-    alignItems: "center",
     justifyContent: "center",
+  },
+  input: {
+    width: "100%",
+    height: 55,
+
+    marginHorizontal: "auto",
+    marginVertical: 20,
+    paddingHorizontal: 10,
+
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: "black",
+  },
+  h1: {
+    marginTop: 20,
+    marginHorizontal: "auto",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  result: {
+    fontWeight: "bold",
   },
 });
--- src/Test.jsx
+++ src/Test.jsx
@@ -3,9 +3,15 @@ import { StyleSheet, Text, View } from "react-native";
 export default function Test() {
   return (
     <View>
-      <Text>ok</Text>
+      <Text style={styles.h1}>Test Component</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  h1: {
+    fontSize: 25,
+    fontWeight: "bold",
+    marginHorizontal: "auto",
+  },
+});
